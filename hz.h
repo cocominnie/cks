c6ee8e3f127acbc5a4440132192a2ddff1d996cd
cocominnie <3271618304@qq.com>
2025-03-19 08:32:20 +0800

    Add files via upload

---
+++ hz.h
@@ -0,0 +1,7 @@
+#ifndef __GHZ_H__
+#define __GHZ_H__
+
+void puthz(int x, int y,char *s,int flag,int part,int color);
+
+#endif
+
---
+++ welcome.h
@@ -0,0 +1,9 @@
+#ifndef _WELCOME_H_
+#define _WELCOME_H_
+
+#include"allfunc.h"
+
+void welcome_text();
+void welcome_page();
+
+#endif // !_WELCOME_H_
