65027e5d877ab940cb81e61d46ba57f322cec749
cocominnie <3271618304@qq.com>
2025-03-20 20:17:04 +0800

    Add files via upload

--- hz.h
+++ hz.h
@@ -3,5 +3,8 @@
 
 void puthz(int x, int y,char *s,int flag,int part,int color);
 
+void prt_asc16_ch(int x, int y, char s, unsigned int color);		//输出一个英文函数
+
+void prt_asc16(int x, int y, char* s, unsigned int color);
 #endif
 
